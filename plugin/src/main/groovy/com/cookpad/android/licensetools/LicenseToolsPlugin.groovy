163063278dd041913ff8901bda4e0c135b27a802
Eric Kuck <eric@bluelinelabs.com>
2017-08-11 08:22:59 -0500

    Update LicenseToolsPlugin.groovy

--- plugin/src/main/groovy/com/cookpad/android/licensetools/LicenseToolsPlugin.groovy
+++ plugin/src/main/groovy/com/cookpad/android/licensetools/LicenseToolsPlugin.groovy
@@ -180,8 +180,6 @@ class LicenseToolsPlugin implements Plugin<Project> {
                 content.append(Templates.buildLicenseHtml(libraryInfo));
             } catch (NotEnoughInformationException e) {
                 noLicenseLibraries.add(e.libraryInfo)
-            } catch (FileNotFoundException ignored) {
-               
             }
         }
 
